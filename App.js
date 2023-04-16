3bb35355cfa1c39db05a46b7012dd180f4decd9d
nitishrestha <nitishrestha8848@gmail.com>
2023-04-16 22:00:23 +0545

    text field

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
+import React, {useState} from 'react';
 
-import React from 'react';
-
-import {
-  Button,
-  Text,
-  View,
-} from 'react-native';
+import {Text, TextInput, View, StyleSheet, Button} from 'react-native';
 
 const App = () => {
-  return (
-    <View>     
-      <Text style={{fontSize:30}}>Styles in react native</Text>
+  const [name, setName] = useState('');
 
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Handle Text Input</Text>
+      <TextInput
+        placeholder=" enter your name"
+        style={styles.textInput}
+        onChangeText={text => setName(text)}
+        value={name}></TextInput>
+      <Text>Your name is : {name}</Text>
+      <Button title="Clear" onPress={() => setName('')}></Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  textInput: {
+    fontSize: 18,
+    color: 'green',
+    borderWidth: 2,
+    borderColor: 'blue',
+    margin: 10,
+  },
+});
 
 export default App;
