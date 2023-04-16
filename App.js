0877b6cd1aa45c70f0d592a3170237a60d269330
nitishrestha <nitishrestha8848@gmail.com>
2023-04-16 21:37:49 +0545

    main

--- App.js
+++ App.js
@@ -1,44 +1,19 @@
 
 import React from 'react';
-import EXstyle from './styles/style'
 
 import {
   Button,
   Text,
   View,
-  StyleSheet
 } from 'react-native';
-import CompanyData from './components/CompanyData'
 
 const App = () => {
   return (
-    <View>
-      {/* Style only a particular component inside a single file is internal style */}
+    <View>     
       <Text style={{fontSize:30}}>Styles in react native</Text>
 
-
-      <Text style={style.textBox}>Styles in react native</Text>
-      <Text style={EXstyle.textBox2}>Styles in react native</Text>
-      <Text style={style.textBox}>Styles in react native</Text>
-      <Text style={EXstyle.textBox2}>Styles in react native</Text> 
     </View>
   );
 };
-
-const style = StyleSheet.create({
-  textBox:{
-    color: 'black',
-    fontSize:20,
-     backgroundColor: 'white',
-     marginTop:4,
-     textAlign:'center'
-  },
-  textBox2:{
-    color: 'white',
-    fontSize:30,
-     backgroundColor: 'black',
-     marginTop:4
-  }
-})
 
 export default App;
