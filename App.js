3c8cf4c1084a5f35eb058d359efcbfb227209448
nitishrestha <nitishrestha8848@gmail.com>
2023-04-20 21:50:21 +0545

    flat list and flat list note

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import React from 'react';
 
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
 
 const App = () => {
+  const users = [
+    {
+      id: 1,
+      name: 'Nitish',
+    },
+    {
+      id: 2,
+      name: 'Madhu',
+    },
+    {
+      id: 3,
+      name: 'Satish',
+    },
+    {
+      id: 4,
+      name: 'Prabin',
+    },
+    {
+      id: 5,
+      name: 'Avinash',
+    },
+    {
+      id: 6,
+      name: 'Sumin',
+    },
+    {
+      id: 7,
+      name: 'Rohan',
+    },
+    {
+      id: 8,
+      name: 'John',
+    },
+  ];
+
   return (
     <View>
-      <Text style={{fontSize: 30}}>Styles in react native</Text>
+      <Text style={{fontSize: 30}}>List with map function in react native</Text>
+      <ScrollView style={style.scroll}>
+        {users.map(user => (
+          <Text style={style.item}>{user.name}</Text>
+        ))}
+      </ScrollView>
     </View>
   );
-
-  const style = StyleSheet.create({
-    textBox: {
-      color: 'black',
-      fontSize: 20,
-      backgroundColor: 'white',
-      marginTop: 4,
-      textAlign: 'center',
-    },
-  });
 };
+
+const style = StyleSheet.create({
+  item: {
+    color: 'black',
+    fontSize: 20,
+    backgroundColor: 'white',
+    marginTop: 4,
+    height: 120,
+    textAlign: 'center',
+  },
+  scroll: {
+    marginBottom: 80,
+  },
+});
 
 export default App;
