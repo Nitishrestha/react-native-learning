dc70b6d6d153cd65a24688217d2cfe7c82830ac2
nitishrestha <nitishrestha8848@gmail.com>
2023-04-20 21:10:34 +0545

    added style constant to the app.js

--- App.js
+++ App.js
@@ -8,6 +8,16 @@ const App = () => {
       <Text style={{fontSize: 30}}>Styles in react native</Text>
     </View>
   );
+
+  const style = StyleSheet.create({
+    textBox: {
+      color: 'black',
+      fontSize: 20,
+      backgroundColor: 'white',
+      marginTop: 4,
+      textAlign: 'center',
+    },
+  });
 };
 
 export default App;
