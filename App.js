b26cc59b2c6bc9c003c6dcbb136fdbd544ca7b7b
nitishrestha <nitishrestha8848@gmail.com>
2023-04-20 22:22:05 +0545

    commit mesz

--- App.js
+++ App.js
@@ -1,23 +1,59 @@
 import React from 'react';
 
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView} from 'react-native';
+
+import users from './User';
 
 const App = () => {
   return (
     <View>
-      <Text style={{fontSize: 30}}>Styles in react native</Text>
+      <Text style={{fontSize: 30}}>Grid with dynamic data</Text>
+      {/* <View style={style.viewStyle}>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+        <Text style={style.viewText}>Sam</Text>
+      </View> */}
+      <View style={style.viewStyle}>
+        {users.map(user => (
+          <Text style={style.viewText}>{user.name}</Text>
+        ))}
+      </View>
     </View>
   );
-
-  const style = StyleSheet.create({
-    textBox: {
-      color: 'black',
-      fontSize: 20,
-      backgroundColor: 'white',
-      marginTop: 4,
-      textAlign: 'center',
-    },
-  });
 };
+
+const style = StyleSheet.create({
+  textBox: {
+    color: 'black',
+    fontSize: 20,
+    backgroundColor: 'white',
+    marginTop: 4,
+    textAlign: 'center',
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  viewText: {
+    fontSize: 25,
+    backgroundColor: 'grey',
+    color: 'white',
+    height: 120,
+    width: 120,
+    margin: 4,
+    padding: 10,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  scroll: {
+    marginBottom: 100,
+  },
+});
 
 export default App;
